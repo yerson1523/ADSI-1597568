c9bd7fafc7a47ded72fc6495dd8a36ffa7e9ecfa
Yerson Andrés Ramírez García <yaramirez352@misena.edu.co>
2018-04-19 02:04:12 -0500

    My first project with php !!

---
+++ Análisis/PHP/MiPrimerProyecto/index.php
@@ -0,0 +1,14 @@
+
+ <?php
+      
+
+
+require 'message1_and_message2.php';
+
+use MyFirstProject\message1_and_message2;
+
+$message1_and_message2 = new message1_and_message2();
+$message1_and_message2->message1();
+$message1_and_message2->message2();
+?>
+ 
---
+++ Análisis/PHP/MiPrimerProyecto/message1_and_message2.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace MyFirstProject;
+
+class message1_and_message2 {
+
+  public function message1() {
+    echo '1. Hello, My name is: Yerson Andrés Ramírez García.';
+    echo '<br>';
+  }
+  public function message2() {
+    echo '2. This is my second message in PHP through object-oriented programming.';
+  }
+}
+        ?>
+
