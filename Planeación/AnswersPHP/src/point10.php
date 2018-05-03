7df3626d177c387d11cb7f724430836df8deb7be
Yerson Andrés Ramírez García <38362344+yerson1523@users.noreply.github.com>
2018-05-02 19:47:53 -0500

    Add files via upload

---
+++ Planeación/AnswersPHP/src/point10.php
@@ -0,0 +1,8 @@
+<?php
+define("CONSTANTE", "definition of the constant.");
+echo CONSTANTE; // muestra "definition of the constant."
+echo '<br>';
+echo CONSTANTE; // muestra "Constant" A constant is an identifier (name) for a simple value. .
+
+
+?>
